20705ac31ad655ad992272bcef582cf1bff329e3
Alexander Aprelev <alexander@aprelev.com>
2014-07-11 00:53:33 -0400

    Adding metadata test

---
+++ test/metadata_test.dart
@@ -0,0 +1,67 @@
+import 'package:unittest/unittest.dart';
+import 'package:unittest/vm_config.dart';
+
+import 'package:oracledart/oracledart.dart';
+
+void main() {
+  useVMConfiguration();
+  OracleConnection connection = new OracleConnection.connect(
+    "scott",
+    "tiger",
+    "(DESCRIPTION="
+      "(ADDRESS=(PROTOCOL=TCP)(HOST=w8-32-12core)(PORT=1521))"
+      "(CONNECT_DATA=(SERVICE_NAME=XE)(SERVER=DEDICATED)))");
+
+  test('Test getMetadataVector', () {
+    OracleResultset resultset =
+        connection.select("select empno, ename from emp");
+    OracleMetadataVector metadata =
+        resultset.getMetadataVector();
+    expect(metadata.getSize(), equals(2));
+    expect(metadata.getColumnName(0), equals("EMPNO"));
+    expect(metadata.getColumnName(1), equals("ENAME"));
+  });
+
+  test('Test getMetadataVector with star', () {
+    OracleResultset resultset =
+        connection.select("select * from emp");
+    OracleMetadataVector metadata =
+        resultset.getMetadataVector();
+    expect(metadata.getSize(), equals(8));
+    expect(metadata.getColumnName(0), equals("EMPNO"));
+    expect(metadata.getColumnName(1), equals("ENAME"));
+    expect(metadata.getColumnName(2), equals("JOB"));
+    expect(metadata.getColumnName(3), equals("MGR"));
+    expect(metadata.getColumnName(4), equals("HIREDATE"));
+    expect(metadata.getColumnName(5), equals("SAL"));
+    expect(metadata.getColumnName(6), equals("COMM"));
+    expect(metadata.getColumnName(7), equals("DEPTNO"));
+  });
+
+  test('Test getMetadataVector iterating through columns', () {
+    OracleResultset resultset =
+        connection.select("select * from emp order by empno");
+    OracleMetadataVector metadata = resultset.getMetadataVector();
+    var columns = {};
+    for (int i = 0; i < metadata.getSize(); i++) {
+      columns[metadata.getColumnName(i)] = i + 1;
+    }
+    resultset.next();
+    expect(resultset.getString(columns["ENAME"]), "SMITH");
+    expect(resultset.getString(columns["JOB"]), "CLERK");
+  });
+
+  test('Test getStringByName, getIntByName', () {
+    OracleResultset resultset =
+        connection.select("select empno, ename from emp where EMPNO='7369'");
+    resultset.next();
+    expect(resultset.getString(1), equals("7369"));
+    expect(resultset.getStringByName("EMPNO"), equals("7369"));
+    expect(resultset.getInt(1), equals(7369));
+    expect(resultset.getIntByName("EMPNO"), equals(7369));
+    expect(resultset.getString(2), equals("SMITH"));
+    expect(resultset.getStringByName("ENAME"), equals("SMITH"));
+  });
+
+  
+}
